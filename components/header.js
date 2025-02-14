3278fa29d042150e376b60707cf5fe6df8a4040d
Welcome Mncube <39636648+welcomemncube@users.noreply.github.com>
2025-02-14 03:50:30 +0200

    Add files via upload

--- components/header.js
+++ components/header.js
@@ -11,22 +11,30 @@ import {
   UserAvatar,
   Settings,
 } from "@carbon/icons-react";
-import { Button } from "@carbon/react";
+import { Menu, MenuItem } from "@mui/material";
+import { Button, IconButton } from "@carbon/react";
 import { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
 
 export default function Header() {
-
-  const [userName, setUsername] = useState('') ;
+  const [userName, setUsername] = useState("");
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   useEffect(() => {
-      setUsername(localStorage.getItem('username'))
-  },[])
+    setUsername(localStorage.getItem("username"));
+  }, []);
 
   const logout = () => {
-    localStorage.removeItem('username')
-    window.location.href = '/login'
-  }
+    localStorage.removeItem("username");
+    window.location.href = "/login";
+  };
 
   return (
     <nav className="navbar">
@@ -61,8 +69,8 @@ export default function Header() {
                 <Document className="icon" />
               </div>
               <div>
-                <Link href="/" className="link">
-                  Licensing
+                <Link href="/apply" className="link">
+                  Apply for Permit
                 </Link>
               </div>
             </div>
@@ -85,8 +93,8 @@ export default function Header() {
                 <Result className="icon" />
               </div>
               <div>
-                <Link href="/" className="link">
-                  News
+                <Link href="/dashboard" className="link">
+                  Dashboard
                 </Link>
               </div>
             </div>
@@ -103,41 +111,87 @@ export default function Header() {
               </div>
             </div>
           </div>
-          {
-            userName ? (
-              <>
-                <div className="block">
-                  <div style={{ display: "flex" }}>
-                <div>
-                  <Settings className="icon" />
-                  <UserAvatar className="icon" />
-                </div>
-                <div style={{marginTop: '2px'}}>
-                  <span>{userName}</span>
-                </div>
-                <Button size="sm" style={{padding: "5px 15px",marginTop: "-5px", borderRadius: "5px",marginLeft: '10px'}} onClick={logout}>Logout</Button>
-                </div>
-              </div>
-              </>
-            ) :
-            <>    
+          {userName ? (
+            <>
               <div className="block">
-              <div >
-              <div>
-                <Button kind="tertiary" size="sm" style={{padding: "5px 15px",marginTop: "-10px", borderRadius: "5px"}}><Link href='/register' className="btn-link">Register</Link></Button>
-              </div>
-            </div>
-          </div>
-          <div className="block">
-            <div >
-              <div>
-                <Button kind="tertiary" size="sm" style={{padding: "5px 15px",marginTop: "-10px", borderRadius: "5px"}}><Link href='/login' className="btn-link">Login</Link></Button>
-              </div>
-            </div>
-          </div>
-            </>
-          }
+                <div style={{ display: "flex" }}>
+                  <div>
+                    <UserAvatar
+                      className="icon"
+                      onClick={handleClick}
+                      id="demo-positioned-button"
+                      aria-controls={open ? "demo-positioned-menu" : undefined}
+                      aria-haspopup="true"
+                      aria-expanded={open ? "true" : undefined}
+                      style={{cursor: 'pointer'}}
+                    />
+                    <div style={{marginTop: '-20px',marginLeft: '20px'}}><span>{userName}</span></div>
+                    
 
+                    <Menu
+                      id="demo-positioned-menu"
+                      aria-labelledby="demo-positioned-button"
+                      anchorEl={anchorEl}
+                      open={open}
+                      onClose={handleClose}
+                      anchorOrigin={{
+                        vertical: "top",
+                        horizontal: "left",
+                      }}
+                      transformOrigin={{
+                        vertical: "top",
+                        horizontal: "left",
+                      }}
+                    >
+                      <MenuItem onClick={handleClose}>Edit Profile</MenuItem>
+                      <MenuItem onClick={logout}>Logout</MenuItem>
+                    </Menu>
+                  </div>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="block">
+                <div>
+                  <div>
+                    <Button
+                      kind="tertiary"
+                      size="sm"
+                      style={{
+                        padding: "5px 15px",
+                        marginTop: "-10px",
+                        borderRadius: "5px",
+                      }}
+                    >
+                      <Link href="/register" className="btn-link">
+                        Register
+                      </Link>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+              <div className="block">
+                <div>
+                  <div>
+                    <Button
+                      kind="tertiary"
+                      size="sm"
+                      style={{
+                        padding: "5px 15px",
+                        marginTop: "-10px",
+                        borderRadius: "5px",
+                      }}
+                    >
+                      <Link href="/login" className="btn-link">
+                        Login
+                      </Link>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </nav>
